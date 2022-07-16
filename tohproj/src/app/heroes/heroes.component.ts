76ae41bc0cd2aaa2713c31af3ef95667fa700490
Surya Prakash S <suryas190299@gmail.com>
2022-07-16 17:30:07 +0530

    parent to child data communication

--- tohproj/src/app/heroes/heroes.component.ts
+++ tohproj/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 import { HEROES } from '../heroes';
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,14 @@ import { HEROES } from '../heroes';
 export class HeroesComponent implements OnInit {
   
   heroes=HEROES;
+  selectedHero:Hero | undefined;
   constructor() { 
   
   }
-  
-  ngOnInit(): void {
-    
+  onClick(hero:Hero):void{
+    this.selectedHero=hero;
+  }
+  ngOnInit(): void {    
   }
 
 }
