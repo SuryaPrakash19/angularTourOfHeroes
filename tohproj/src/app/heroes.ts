b94a5910e48a6728e91b56cb8739af2a695f9cac
Surya Prakash S <suryas190299@gmail.com>
2022-07-16 15:02:42 +0530

    ngFor added to an li element to make it repeat with heroes list

---
+++ tohproj/src/app/heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "Surya" },
+    { id: 2, name: "Kash" },
+    { id: 3, name: "Otta" },
+    { id: 4, name: "Don" },
+    { id: 5, name: "Kuruvi" },
+    { id: 6, name: "Prakash" },
+    { id: 7, name: "SP" },
+    { id: 8, name: "Surya" },
+];
--- tohproj/src/app/heroes/heroes.component.ts
+++ tohproj/src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { HEROES } from '../heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,7 +7,7 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   
-  hero:Hero={id: 1,name:"Surya"};  
+  heroes=HEROES;
   constructor() { 
   
   }
